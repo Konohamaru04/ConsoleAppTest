cecbeec2f19c5b37396825706ccfd58e57403c2e
Amartya Deshmukh <dedsec.konohamaru@gmail.com>
2022-11-11 12:54:57 +0530

    ConfigManagerExample

---
+++ DbClassLib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Configuration;
+
+namespace DbClassLib
+{
+    public class DbClass
+    {
+        public string GetConnString()
+        {
+            string configvalue1 = ConfigurationManager.AppSettings["strSql"];
+            return configvalue1;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DbClassLib;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbClass dbClass = new DbClass();
+            Console.WriteLine(dbClass.GetConnString());
+        }
+    }
+}
